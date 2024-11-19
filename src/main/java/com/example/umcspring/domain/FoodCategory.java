14f8690fbf753cf44f2f4645bd81321da92d30c3
sese2204 <sese2204@naver.com>
2024-11-19 14:02:23 +0900

    refactor: QDSL을 위한 gradle 파일 설정 및 Region 클래스 어노테이션 추가

--- src/main/java/com/example/umcspring/domain/FoodCategory.java
+++ src/main/java/com/example/umcspring/domain/FoodCategory.java
@@ -1,10 +1,15 @@
 package com.example.umcspring.domain;
 
 import com.example.umcspring.domain.common.BaseEntity;
+import com.example.umcspring.domain.mapping.MemberMission;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -21,4 +26,7 @@ public class FoodCategory extends BaseEntity {
     private Long id;
 
     private String name;
+
+    //@OneToMany(mappedBy = "food_category", cascade = CascadeType.ALL)
+    //private List<MemberMission> foodCategoryList = new ArrayList<>();
 }

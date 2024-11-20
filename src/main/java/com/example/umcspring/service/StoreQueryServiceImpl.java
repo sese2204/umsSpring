ae0b5afd98aeda91e5fd606e4d22df01c113d293
sese2204 <sese2204@naver.com>
2024-11-20 18:58:23 +0900

    feat(service): QDSL 사용을 위한 service 구현 및 UmcSpringApplication 클래스에서 조립

---
+++ src/main/java/com/example/umcspring/service/StoreQueryServiceImpl.java
@@ -0,0 +1,31 @@
+package com.example.umcspring.service;
+
+import com.example.umcspring.domain.Store;
+import com.example.umcspring.repository.StoreRepository;
+import java.util.List;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class StoreQueryServiceImpl implements StoreQueryService{
+
+    private final StoreRepository storeRepository;
+
+    @Override
+    public Optional<Store> findStore(Long id) {
+        return storeRepository.findById(id);
+    }
+
+    @Override
+    public List<Store> findStoresByNameAndScore(String name, Float score) {
+        List<Store> filteredStores = storeRepository.dynamicQueryWithBooleanBuilder(name, score);
+
+        filteredStores.forEach(store -> System.out.println("Store: " + store));
+
+        return filteredStores;
+    }
+}

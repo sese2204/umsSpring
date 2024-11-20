4cad36b4f0dd5462f370c63effe77c82fa421014
sese2204 <sese2204@naver.com>
2024-11-20 18:56:58 +0900

    refactor(domain): 도메인에 필요한필드와 메소드 추가

--- src/main/java/com/example/umcspring/domain/Store.java
+++ src/main/java/com/example/umcspring/domain/Store.java
@@ -2,9 +2,12 @@ package com.example.umcspring.domain;
 
 import com.example.umcspring.domain.common.BaseEntity;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -26,4 +29,19 @@ public class Store extends BaseEntity {
     private String address;
 
     public Float score;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "region_id")
+    private Region region;
+
+    @Override
+    public String toString() {
+        return "Store{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                ", score=" + score +
+                ", region=" + region +
+                '}';
+    }
 }

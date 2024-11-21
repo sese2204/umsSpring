f2028a1c7d33d283b797567ee70abfdd6b112165
sese2204 <sese2204@naver.com>
2024-11-21 15:48:41 +0900

    feat: 테스트를 위한 service 및 controller 조립

--- src/main/java/com/example/umcspring/web/controller/TempRestController.java
+++ src/main/java/com/example/umcspring/web/controller/TempRestController.java
@@ -2,20 +2,30 @@ package com.example.umcspring.web.controller;
 
 import com.example.umcspring.apiPayload.ApiResponse;
 import com.example.umcspring.converter.TempConverter;
+import com.example.umcspring.service.tempService.TempQueryService;
 import com.example.umcspring.web.dto.TempResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/temp")
 @RequiredArgsConstructor
 public class TempRestController {
+    private final TempQueryService tempQueryService;
 
     @GetMapping("/test")
     public ApiResponse<TempResponse.TempTestDTO> testAPI(){
-
         return ApiResponse.onSuccess(TempConverter.toTempTestDTO());
+    }
+
+    @GetMapping("/exception")
+    public ApiResponse<TempResponse.TempExceptionDTO> exceptionAPI(
+            @RequestParam("flag") Integer flag
+    ) {
+        tempQueryService.CheckFlag(flag);
+        return ApiResponse.onSuccess(TempConverter.toTempExceptionDTO(flag));
     }
 }
